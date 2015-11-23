3c32ef88f98e605fbc5873a6e198e85a1e2d31e2
Nick <nicholas.condo@gmail.com>
2015-11-23 17:56:52 -0500

    Add function make_dir to create the directories to save photos

--- reddit_imgur.py
+++ reddit_imgur.py
@@ -1,10 +1,18 @@
 """
 
-This program downloads the top rated images of the day from a user specified 
-subreddit. Images are saved in the directory in which the program is run,
-in a folder named after the specified subreddit.
+This program downloads the top rated images of the day from a user 
+specified subreddit. Images are saved in the directory in which the 
+program is run, in a folder named after the specified subreddit.
 """
 
 __author__ = "Nick Condo"
 __date__ = "11/22/2015"
+
+import praw, os
+
+def make_dir(dir):
+    """Creates the directory to save the photos if it doesn't already exist"""
+    if not os.path.exists(dir):
+        os.marked(dir)
+
 
