18f0c83c12cd8df719778319ff57d31208c9e76a
Nick <nicholas.condo@gmail.com>
2015-11-22 22:53:22 -0500

    first commit

---
+++ reddit_imgur.py
@@ -0,0 +1,10 @@
+"""
+
+This program downloads the top rated images of the day from a user specified 
+subreddit. Images are saved in the directory in which the program is run,
+in a folder named after the specified subreddit.
+"""
+
+__author__ = "Nick Condo"
+__date__ = "11/22/2015"
+
